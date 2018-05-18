b6fc6c2cd145fe8f6f4f78670ce0f869cd135294
Habib Qureshi <mytrueemailaddress@gmail.com>
2018-05-17 21:53:59 -0500

    Update youtubeTimeKeeper.js

--- youtubeTimeKeeper.js
+++ youtubeTimeKeeper.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         YT Time redux 7
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/watch?v*
+// @grant        none
+// ==/UserScript==
+
 setInterval(function(){window.history.replaceState({}, "",
   window.location.href.slice(0, 43)
   + "&t=" 
@@ -6,4 +16,5 @@ setInterval(function(){window.history.replaceState({}, "",
   + Math.floor(document.querySelector("#movie_player").getCurrentTime() / 60 % 60) 
   + "m" 
   + Math.floor(document.querySelector("#movie_player").getCurrentTime() % 60) 
-  + "s")}, 1000);
+  + "s")}
+, 1000);
